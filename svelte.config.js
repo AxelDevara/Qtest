68d4e0628044610954f2551c18cdf2bf5a812f1a
AxelDevara <axeldevara@gmail.com>
2024-10-01 20:01:38 +0700

    fixed version

--- svelte.config.js
+++ svelte.config.js
@@ -13,11 +13,8 @@ const config = {
     adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			edge: 'false',
+			split: 'false',
 		}),
     alias: {
       '@pages': path.resolve('./src/pages'),
